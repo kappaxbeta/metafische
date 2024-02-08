b8a04726718f90d75277c7ba968700d12e2ce2c5
kappaxbeta <jens.boesche@me.com>
2024-02-08 19:05:47 +0100

    Add Metafisch/e start with a lot of deeling

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -49,8 +49,8 @@ const Footer = () => (
                 src={social.url}
                 alt={social.name}
                 className="w-[24px] h-[24px] object-contain cursor-pointer"
-
-              /></a>
+              />
+              </a>
             ))}
           </div>
         </div>
