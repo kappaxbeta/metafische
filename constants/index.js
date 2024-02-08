fbcfac1ad0304c099a027afe4b8f0f48067816d7
kappaxbeta <jens.boesche@me.com>
2024-02-08 18:32:31 +0100

    Add Metafisch/e start with a lot of deeling

--- constants/index.js
+++ constants/index.js
@@ -50,19 +50,19 @@ export const newFeatures = [
 export const insights = [
   {
     imgUrl: '/planet-06.png',
-    title: 'The launch of the Metaverse makes Elon musk ketar-ketir',
+    title: 'The launch of the Metafisch/e makes Elon musk ketar-ketir',
     subtitle:
         'Magna etiam tempor orci eu lobortis elementum nibh tellus molestie. Diam maecenas sed enim ut sem viverra alique.',
   },
   {
     imgUrl: '/planet-07.png',
-    title: '7 tips to easily master the madness of the Metaverse',
+    title: '7 tips to easily master the madness of the Metafisch/e',
     subtitle:
         'Vitae congue eu consequat ac felis donec. Et magnis dis parturient montes nascetur ridiculus mus. Convallis tellus id interdum',
   },
   {
     imgUrl: '/planet-08.png',
-    title: 'With one platform you can explore the whole world virtually',
+    title: 'With one platform you can explore the whole space virtually',
     subtitle:
         'Quam quisque id diam vel quam elementum. Viverra nam libero justo laoreet sit amet cursus sit. Mauris in aliquam sem',
   },
@@ -72,17 +72,11 @@ export const socials = [
   {
     name: 'twitter',
     url: '/twitter.svg',
-  },
-  {
-    name: 'linkedin',
-    url: '/linkedin.svg',
+    icon: 'https://twitter.com/metafische',
   },
   {
     name: 'instagram',
     url: '/instagram.svg',
-  },
-  {
-    name: 'facebook',
-    url: '/facebook.svg',
-  },
+    icon: 'https://instagram.com/metafische',
+  }
 ];
--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -16,7 +16,7 @@ const Hero = () => (
     >
       <div className="flex justify-center items-center flex-col relative z-10">
         <motion.h1 variants={textVariant(1.1)} className={styles.heroHeading}>
-          Metaverse
+          Metafisch/e
         </motion.h1>
         <motion.div
           variants={textVariant(1.2)}
@@ -34,24 +34,21 @@ const Hero = () => (
       >
         <div className="absolute w-full h-[300px] hero-gradient rounded-tl-[140px] z-[0] -top-[30px]" />
 
-        <img
-          src="/cover.png"
-          alt="hero_cover"
-          className="w-full sm:h-[500px] h-[350px] object-cover rounded-tl-[140px] z-10 relative"
-        />
-
-        <a href="#explore">
-          <div className="w-full flex justify-end sm:-mt-[70px] -mt-[50px] pr-[40px] relative z-10">
-            <img
-              src="/stamp.png"
-              alt="stamp"
-              className="sm:w-[155px] w-[100px] sm:h-[155px] h-[100px] object-contain"
-            />
-          </div>
+        <a href="#explore" className={"bg-pink-600 text-amber-300 font-bold font-extrabold p-1 border-green-400 border "}>
+          #DON'TPANIC
         </a>
       </motion.div>
     </motion.div>
   </section>
 );
 
+/*
+
+        <img
+          src="/cover.png"
+          alt="hero_cover"
+          className="w-full sm:h-[500px] h-[350px] object-cover rounded-tl-[140px] z-10 relative"
+        />
+
+* */
 export default Hero;
