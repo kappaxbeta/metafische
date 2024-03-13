e941d70d9623d8cf33afdb43c2fcd55981734c7b
kappaxbeta <jens.boesche@me.com>
2024-03-13 17:48:00 +0100

    get serius

--- sections/Feedback.jsx
+++ sections/Feedback.jsx
@@ -21,17 +21,16 @@ const Feedback = () => (
         <div className="feedback-gradient" />
         <div>
           <h4 className="font-bold sm:text-[32px] text-[26px] sm:leading-[40.32px] leading-[36.32px] text-white">
-            Samantha
+            Jens
           </h4>
           <p className="mt-[8px] font-normal sm:text-[18px] text-[12px] sm:leading-[22.68px] leading-[16.68px] text-white">
-            Founder Metaverus
+            Tester
           </p>
         </div>
 
         <p className="mt-[24px] font-normal sm:text-[24px] text-[18px] sm:leading-[45.6px] leading-[39.6px] text-white">
           “With the development of today's technology, Metafisch/e is very
-          useful for today's work, or can be called web 3.0. by using
-          Metafisch/e you can use it as anything”
+          useful"
         </p>
       </motion.div>
 
