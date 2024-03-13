470498a9fd2e5fae09f7a464ba08ea75b9644160
kappaxbeta <jens.boesche@me.com>
2024-03-13 16:45:54 +0100

    get serius

--- app/metarect/page.js
+++
@@ -1,7 +0,0 @@
-function Page() {
-    return <div>
-        <spline-viewer loading-anim-type="spinner-small-dark"
-          url="https://prod.spline.design/ArBOHr-5lRVgXTbO/scene.splinecode"/>
-    </div>;
-}
-
